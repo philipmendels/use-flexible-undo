0796fd77a0ff7c2a56bcb7705649479cfdf6ee2d
Philip Mendels <p.mendels@gmail.com>
2019-12-27 14:09:49 +0100

    Add initial implementation and example

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,35 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { useInfiniteUndo } from '../.';
+import { useState, useMemo } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const { undo, redo, makeUndoable } = useInfiniteUndo();
+  const increment = makeUndoable<number>({
+    type: 'INCREMENT',
+    do: n => setCount(count => count + n),
+    undo: n => setCount(count => count - n),
+  });
+
+  const decrement = useMemo(
+    () =>
+      makeUndoable<number>({
+        type: 'DECREMENT',
+        do: n => setCount(count => count - n),
+        undo: n => setCount(count => count + n),
+      }),
+    [makeUndoable]
+  );
+
   return (
     <div>
-      <Thing />
+      <button onClick={() => increment(3)}> increment </button>
+      <button onClick={() => decrement(2)}> decrement </button>
+      <button onClick={() => undo()}> undo </button>
+      <button onClick={() => redo()}> redo </button>
+      count: {count}
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,60 @@
-import * as React from 'react';
+import { useState, useCallback, useRef } from 'react';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+export interface UndoableAction<P> {
+  type: string;
+  describe?: (payload: P) => string;
+  do: (payload: P) => void;
+  undo: (payload: P) => void;
+}
+
+type ActionsRecord = Record<string, UndoableAction<any>>;
+
+interface StackItem {
+  type: string;
+  payload: any;
+}
+
+export const useInfiniteUndo = () => {
+  const actionsRef = useRef<ActionsRecord>({});
+
+  const [past, setPast] = useState<StackItem[]>([]);
+  const [future, setFuture] = useState<StackItem[]>([]);
+
+  const undo = useCallback(() => {
+    if (past.length) {
+      const item = past[past.length - 1];
+      actionsRef.current[item.type].undo(item.payload);
+      setPast(past.slice(0, past.length - 1));
+      setFuture(future => [item, ...future]);
+    }
+  }, [past]);
+
+  const redo = useCallback(() => {
+    if (future.length) {
+      const [item, ...rest] = future;
+      actionsRef.current[item.type].do(item.payload);
+      setFuture(rest);
+      setPast(past => [...past, item]);
+    }
+  }, [future]);
+
+  const makeUndoable = useCallback(
+    <P extends any>(action: UndoableAction<P>) => {
+      const { type } = action;
+      console.log('MAKE UNDOABLE', type);
+      actionsRef.current[type] = action;
+      return (payload: P) => {
+        action.do(payload);
+        setPast(past => [...past, { type, payload }]);
+        setFuture([]);
+      };
+    },
+    []
+  );
+
+  return {
+    makeUndoable,
+    undo,
+    redo,
+  };
 };
